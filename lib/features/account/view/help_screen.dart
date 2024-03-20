012a895871233be43d04a9d608d78fedf7c1a020
Amjad ali kasim.T <amjadalikasim4@gmail.com>
2024-03-20 11:38:10 +0530

    #46 sui

--- lib/features/account/view/help_screen.dart
+++ lib/features/account/view/help_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HelpScreen extends StatelessWidget {
-  const HelpScreen({super.key});
+  const HelpScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,9 @@ class HelpScreen extends StatelessWidget {
           children: [
             ListTile(
               title: const Text('Contact Us'),
-              onTap: () {},
+              onTap: () {
+                showSupportMessage(context);
+              },
             ),
             ListTile(
               title: const Text('Privacy Policy'),
@@ -30,5 +33,73 @@ class HelpScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+void showSupportMessage(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              icon: Icon(Icons.arrow_back_ios, size: 18),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            const Text('Contact Support'),
+            SizedBox(
+              width: 30,
+            )
+          ],
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Whatsapp Number: +91 1234567890',
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                launchWhatsapp('https://wa.me/1234567890');
+              },
+              child: const Text('Whatsapp'),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Support Email: support@example.com',
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                launchEmail('mailto:support@example.com');
+              },
+              child: const Text('Email'),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+void launchWhatsapp(String url) {
+  launchUrl(url);
+}
+
+void launchEmail(String url) {
+  launchUrl(url);
+}
+
+Future<void> launchUrl(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
