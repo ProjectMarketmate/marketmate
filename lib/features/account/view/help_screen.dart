3dd2bfc71a1c22cb7d04d1f554886f464f667198
Amjad ali kasim.T <amjadalikasim4@gmail.com>
2024-03-20 10:13:49 +0530

    #38 done

---
+++ lib/features/account/view/help_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HelpScreen extends StatelessWidget {
+  const HelpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Help & Support'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text('Contact Us'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Privacy Policy'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('Terms & Conditions'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
